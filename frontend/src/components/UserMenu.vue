02442ee7c4e54d5fface88fe1494a0e9c3a19bc1
endhyarnie <endhyarnie@gmail.com>
2021-03-13 09:34:19 +0800

    .

--- frontend/src/components/UserMenu.vue
+++ frontend/src/components/UserMenu.vue
@@ -1,42 +1,37 @@
 <template>
   <v-card
     class="mx-auto"
-    max-width="500"
+    max-width="350"
   >
-
     <v-list dense>
-      <v-list-item-group
-        v-model="selectedItem"
-        color="primary"
+      <v-list-item
+        v-for="(item, i) in Beranda"
+        :key="i"
+        link
       >
-        <v-list-item
-          v-for="(item, i) in Beranda"
-          :key="i"
-        >
-            
-            <v-list-item-icon>
-              <v-checkbox class="mt-n1"></v-checkbox>
-              <!-- <v-icon v-text="item.icon"></v-icon> -->
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item-content>
-
-        </v-list-item>
-      </v-list-item-group>
-
+        <v-list-item-icon>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-icon>
+        <div class="mr-3">
+          <input type="checkbox" v-text="item.title">
+        </div>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      
       <v-list-group
-        v-for="item in items"
+        v-for="item in Master"
         :key="item.title"
         v-model="item.active"
-        
+        :prepend-icon="item.action"
         no-action
       >
-        <!-- :prepend-icon="item.action" -->
         <template v-slot:activator>
-          <v-checkbox></v-checkbox>
+          <div class="mr-3">
+            <input type="checkbox" v-text="item.title" v-model="item.active">
+          </div>
           <v-list-item-content>
-
             <v-list-item-title v-text="item.title"></v-list-item-title>
           </v-list-item-content>
         </template>
@@ -46,7 +41,9 @@
           :key="child.title"
           link
         >
-          <v-checkbox></v-checkbox>
+          <div class="mr-3">
+            <input type="checkbox" v-text="child.title" v-model="child.active">
+          </div>
           <v-list-item-content>
             <v-list-item-title v-text="child.title"></v-list-item-title>
           </v-list-item-content>
@@ -63,7 +60,7 @@
         { text: 'Beranda', icon: 'mdi-home' },
       ],
 
-      items: [
+      Master: [
         {
           action: 'mdi-database',
           items: [
